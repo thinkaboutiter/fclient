bb9aad66f6b694382a4054409b4b60d0716e4cda
Boyan Yankov <boian.iankov@gmail.com>
2016-06-21 20:03:32 +0300

    added inital project

---
+++ FClient/FClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FClient
+//
+//  Created by Boyan Yankov on W25 21/06/2016 Tue.
+//  Copyright © 2016 Boyan Yankov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

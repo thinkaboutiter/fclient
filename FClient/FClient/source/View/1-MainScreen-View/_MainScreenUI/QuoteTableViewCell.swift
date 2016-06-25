0523123666eeed664dd5afe677fe98ec5b44a444
Boyan Yankov <boian.iankov@gmail.com>
2016-06-25 16:29:13 +0300

    updated `QuoteTableViewCell.swift` - added base implementation

--- FClient/FClient/source/View/1-MainScreen-View/_MainScreenUI/QuoteTableViewCell.swift
+++ FClient/FClient/source/View/1-MainScreen-View/_MainScreenUI/QuoteTableViewCell.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import SimpleLogger
+
+protocol QuoteTableViewCellActionConsumable: class {
+    func quoteTableViewCellBuyButtonTapped(sender: QuoteTableViewCell)
+    func quoteTableViewCellSellButtonTapped(sender: QuoteTableViewCell)
+}
 
 class QuoteTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    // bidView
+    @IBOutlet weak var bidLabel: UILabel!
+    @IBOutlet weak var bidOrientationImageView: UIImageView!
+    @IBOutlet weak var sellButton: UIButton!
+    
+    // askView
+    @IBOutlet weak var askLabel: UILabel!
+    @IBOutlet weak var askOrientationImageView: UIImageView!
+    @IBOutlet weak var buyButton: UIButton!
+    
+    private(set) weak var actionConsumableDelegate: QuoteTableViewCellActionConsumable?
+    
+    // MARK: Cascaded Accessors
+    
+    func updateActionConsumableDelegate(newDelegate: QuoteTableViewCellActionConsumable) {
+        self.actionConsumableDelegate = newDelegate
+    }
 
+    // MARK: Life cycle
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.selectionStyle = .None
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -20,5 +52,31 @@ class QuoteTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    func updateUIWithQuote(quote: Quote)  {
+        self.symbolLabel.text = quote.displayName
+        self.bidLabel.text = quote.bid
+        
+        // TODO: configure bidOrientationImageView
+        
+        self.askLabel.text = quote.ask
+        
+        // TODO: configure askOrientationImageView
+        
+        // TODO: configure colors using `changeOrientation` on passed quote parameter
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func sellButtonTapped(sender: UIButton) {
+        Logger.logDebug().logMessage("\(self) \(#line) \(#function) » ")
+        
+        self.actionConsumableDelegate?.quoteTableViewCellSellButtonTapped(self)
+    }
+    
+    @IBAction func buyButtonTapped(sender: UIButton) {
+        Logger.logDebug().logMessage("\(self) \(#line) \(#function) » ")
+        
+        self.actionConsumableDelegate?.quoteTableViewCellBuyButtonTapped(self)
+    }
 }

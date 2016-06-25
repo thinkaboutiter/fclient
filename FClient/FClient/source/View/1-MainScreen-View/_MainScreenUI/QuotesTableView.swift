8c21870088c734687a3a7143e1bcbeece61b1aee
Boyan Yankov <boian.iankov@gmail.com>
2016-06-25 15:42:50 +0300

    added `QuotesTableView.swift`, `QuoteTableViewCell.swift`

---
+++ FClient/FClient/source/View/1-MainScreen-View/_MainScreenUI/QuotesTableView.swift
@@ -0,0 +1,21 @@
+//
+//  QuotesTableView.swift
+//  FClient
+//
+//  Created by Boyan Yankov on W25 25/06/2016 Sat.
+//  Copyright © 2016 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+
+class QuotesTableView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

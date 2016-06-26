80f64ff19a3f6793ea982ab175ae49796eee0673
Boyan Yankov <boian.iankov@gmail.com>
2016-06-26 21:44:44 +0300

    added `QuoteCollectionViewCell.swift`

---
+++ FClient/FClient/source/View/1-MainScreen-View/_MainScreenUI/QuoteCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  QuoteCollectionViewCell.swift
+//  FClient
+//
+//  Created by Boyan Yankov on W25 26/06/2016 Sun.
+//  Copyright © 2016 boyankov@yahoo.com. All rights reserved.
+//
+
+import UIKit
+import SimpleLogger
+
+class QuoteCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: - Life cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
